bd40322b215c75b5669aadc3f3a8aa2884490bc4
Andrii Millovich <triplle@ukr.net>
2024-10-20 17:57:50 +0300

    add create method

--- back/src/repository/repository.ts
+++ back/src/repository/repository.ts
@@ -1,8 +1,12 @@
-import { Repository } from '../common/types/repository/repository';
+import type { Repository } from '../common/types/repository/repository';
 
 class AbstractRepository<T> implements Repository<T> {
 	getAll(query: string): Promise<T[]> {
 		throw new Error('Method not implemented.');
+	}
+
+	create(data: T, query: string): Promise<T> {
+		throw new Error('Method not implemented');
 	}
 }
 
