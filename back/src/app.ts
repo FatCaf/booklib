005a18491eec996d0a59e3d75b9dde0a38df43af
Andrii Millovich <triplle@ukr.net>
2024-10-20 13:27:16 +0300

    update imports

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,1 +1,35 @@
-console.log("Hello World!");
+import express from 'express';
+import { BookModule } from './modules/book-module/module';
+import { config } from './database/config/config';
+import { Database } from './database/database';
+
+class Application {
+	private database: Database;
+	private bookModule: BookModule;
+	private app;
+
+	constructor() {
+		this.database = new Database(config);
+		this.app = express();
+		this.bookModule = new BookModule(this.database, this.app);
+	}
+
+	public async initDb() {
+		await this.database.connect();
+	}
+
+	public async start() {
+		await this.initDb();
+
+		this.bookModule.initModule();
+
+		this.app.listen(3000, () => {
+			console.log('Server running on port 3000');
+		});
+	}
+}
+
+const application = new Application();
+(async () => {
+	await application.start();
+})();
